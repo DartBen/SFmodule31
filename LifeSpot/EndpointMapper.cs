4e96acada2a9c6aaf77dcd6402dff0483aaef156
DESKTOP-BBDMNJN\Dima <ryabchikk@rambler.ru>
2023-04-20 22:25:52 +0300

    исправление ошибок запуска

--- LifeSpot/EndpointMapper.cs
+++ LifeSpot/EndpointMapper.cs
@@ -1,11 +1,8 @@
 ﻿using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace LifeSpot
 {
@@ -54,6 +51,7 @@ namespace LifeSpot
         {
             string footerHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "footer.html"));
             string sideBarHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "sidebar.html"));
+            string sliderHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "slider.html"));
 
             builder.MapGet("/", async context =>
             {
@@ -87,7 +85,9 @@ namespace LifeSpot
                 // Загружаем шаблон страницы, вставляя в него элементы
                 var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
                     .Replace("<!--SIDEBAR-->", sideBarHtml)
-                    .Replace("<!--FOOTER-->", footerHtml);
+                    .Replace("<!--FOOTER-->", footerHtml)
+                    // Добавим для загрузки слайдера
+                   .Replace("<!--SLIDER-->", sliderHtml);
 
                 await context.Response.WriteAsync(html.ToString());
             });
--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -1,8 +1,5 @@
-using System.IO;
-using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -30,6 +27,5 @@ namespace LifeSpot
                 endpoints.MapHtml();
             });
         }
-    }
     }
 }
